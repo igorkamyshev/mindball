eec324f527db36f62b2e93ded6989fd07d2537b4
Igor Kamyshev <garik.novel@gmail.com>
2017-10-06 15:04:46 +0700

    add basic layouts for org

---
+++ src/AppBundle/Controller/OrgController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class OrgController extends Controller
+{
+    /**
+     * @Route("/org", name="org_homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('org/index.html.twig');
+    }
+
+    /**
+     * @Route("/org/tournaments", name="org_tournaments")
+     */
+    public function tournamentsAction()
+    {
+        return $this->render('org/tournaments.html.twig');
+    }
+}
